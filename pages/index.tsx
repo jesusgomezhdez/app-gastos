5dff492f28c1f8e408b6ce390bd0612d56883a0f
Jesús Gómez <gomezhdez.jesus@gmail.com>
2022-10-15 14:47:08 -0500

    Add accion para descargar tabla y fix responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+
+import { createRef } from 'react'
 import type { NextPage } from 'next'
 import {
   Container,
@@ -14,8 +16,12 @@ import {
   Th,
   Tbody,
   Td,
+  Tfoot,
+  TableCaption
 } from '@chakra-ui/react'
 import { useState } from 'react'
+//@ts-ignore
+import { useScreenshot, createFileName } from 'use-react-screenshot'
 
 interface Expense {
   name: string;
@@ -23,6 +29,13 @@ interface Expense {
 }
 
 const Home: NextPage = () => {
+  const ref = createRef<HTMLDivElement>()
+  const [image, takeScreenShot] = useScreenshot({
+    type: "image/jpeg",
+    quality: 1.0
+  });
+
+
   const [inputDate, setInputDate] = useState<string>('')
   const [inputTotalAmount, setInputTotalAmount] = useState<number>(0)
   const [newTotalAmount, setNewTotalAmount] = useState<number>(0)
@@ -49,13 +62,23 @@ const Home: NextPage = () => {
     return numberFormat.format(amount)
   }
 
+  const download = (image: any, { extension = "jpg" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    a.download = createFileName(extension, inputDate);
+    a.click();
+  };
+
+  const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+
+
   return (
     <Container maxW='container.lg' >
       <Box textAlign='center' margin={10} >
         <Heading>Control de Gastos</Heading>
       </Box>
-      <Container maxW='container.md' bg='gray.50' padding={5}>
-        <Stack direction='row'>
+      <Container maxW='container.md' bg='gray.50' padding={5} mb={10}>
+        <Stack direction={['column', 'row']}>
           <Box flex={1}>
             <Text fontSize='sm'>Fecha</Text>
             <Input
@@ -69,7 +92,7 @@ const Home: NextPage = () => {
             />
           </Box>
           <Box flex={1}>
-            <Text 
+            <Text
               fontSize='sm'
             >Monto Total</Text>
             <Input
@@ -98,26 +121,31 @@ const Home: NextPage = () => {
             </Text>
           )}
         </Stack>
-        <Stack direction='row' mt={10}>
-          <Input
-            placeholder='Descripción del gasto'
-            size='md'
-            value={inputDescription}
-            onChange={(e) => {
-              setInputDescription(e.target.value)
-            }}
-            type='text'
-          />
-          <Input
-            placeholder='Monto total'
-            size='md'
-            value={inputAmount}
-            onChange={(e) => {
-              setInputAmount(+e.target.value)
-            }}
-            type='number'
-          />
-          <Box>
+        <Stack direction={['column', 'row']} mt={10}>
+          <Box flex={2}>
+            <Input
+              placeholder='Descripción del gasto'
+              size='md'
+              value={inputDescription}
+              onChange={(e) => {
+                setInputDescription(e.target.value)
+              }}
+              type='text'
+            />
+          </Box>
+          <Box flex={2}>
+            <Input
+              placeholder='Monto total'
+              size='md'
+              value={inputAmount}
+              onChange={(e) => {
+                console.log(e, 'event')
+                setInputAmount(+e.target.value)
+              }}
+              type='number'
+            />
+          </Box>
+          <Box flex={1} justifyContent='center' display='flex' >
             <Button
               colorScheme='twitter'
               size='md'
@@ -128,24 +156,41 @@ const Home: NextPage = () => {
             </Button>
           </Box>
         </Stack>
-        <TableContainer mt={10}>
-          <Table size='sm'>
-            <Thead>
-              <Tr>
-                <Th>Gasto</Th>
-                <Th isNumeric>Monto</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {expensesList.map((expense, index) => (
-                <Tr key={index}>
-                  <Td>{expense.name}</Td>
-                  <Td isNumeric>{formatedNumber(expense.amount)}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
+        {expensesList.length !== 0 ? (
+          <>
+            <Box ref={ref} sx={{ padding: 0 }}>
+              <TableContainer mt={10}>
+                <Table size='sm'>
+                  <Thead>
+                    <Tr>
+                      <Th>Gasto</Th>
+                      <Th isNumeric>Monto</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {expensesList.map((expense, index) => (
+                      <Tr key={index}>
+                        <Td>{expense.name}</Td>
+                        <Td isNumeric>{formatedNumber(expense.amount)}</Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                  <TableCaption>{`${inputDate}  ${formatedNumber(inputTotalAmount)} - ${formatedNumber(newTotalAmount)}`}</TableCaption>
+                </Table>
+              </TableContainer>
+            </Box>
+            <Box flex={1} justifyContent='center' display='flex' mt={10}>
+              <Button
+                colorScheme='twitter'
+                size='md'
+                onClick={downloadScreenshot}
+              >
+                Descargar
+              </Button>
+            </Box>
+          </>
+        ) : null}
+
       </Container>
     </Container>
   )
